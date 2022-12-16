4445c18f17ded3ce60baab5610452e78282e66fa
QRiche <18075475+QRiche@users.noreply.github.com>
2022-12-16 17:55:52 -0500

    add tailwind css + remove prettier

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,14 @@
-<header>
+<script>
+    import "../app.css";
+</script>
+
+<header class="text-center font-semibold text-6xl">
     <h1>Richard Quan</h1>
 </header>
 
 <slot />
 
-<footer>
+<hr class="w-72 mx-auto bg-gray-300 my-5 h-px border-0" />
+<footer class="text-sm text-center">
     <p>© Richard Quan 2022</p>
 </footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="text-center">
+    <div>
+        <h2 class="my-9 text-3xl">
+            Hey <span class="motion-safe:hover:animate-bounce mx-0 my-0"
+                >👋</span
+            >
+        </h2>
+    </div>
+    <p class="my-9 text-xl">
+        My name is <span class="text-blue-600">Richard Quan</span> and I like to
+        learn
+        <a
+            class="text-blue-600 hover:text-sky-400"
+            href="https://github.com/QRiche">things</a
+        >.
+    </p>
+</div>
+<p class="mx-72">
+    I am a second-year Computer Science student at <a
+        class="hover:underline underline-offset-8"
+        href="https://torontomu.ca"
+        ><span
+            class="font-extrabold stroke-black stroke-2 text-transparent bg-clip-text bg-gradient-to-r from-blue-700 via-blue-600 to-yellow-600"
+            >Toronto Metropolitan University</span
+        ></a
+    >. I enjoy building computers, electronics, and programming. Sometimes I
+    like going outside.
+</p>
